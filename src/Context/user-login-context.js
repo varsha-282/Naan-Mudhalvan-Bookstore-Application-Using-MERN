2ea9fa23c4e5b64e2b7be837edd2a1dd1ad71b8c
varsha-282 <155875245+varsha-282@users.noreply.github.com>
2024-11-15 19:57:06 +0530

    src

---
+++ src/Context/user-login-context.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useState, useContext, createContext } from "react"
+
+const UserLoginContext = createContext()
+
+let UserLoginContextProvider = ({children}) => {
+    const [userLoggedIn, setUserLoggedIn] = useState(false)
+    
+    return (
+        <UserLoginContext.Provider value={{userLoggedIn, setUserLoggedIn}}>
+            {children}
+        </UserLoginContext.Provider>
+    )
+}
+
+const useUserLogin = () => useContext(UserLoginContext)
+
+export { UserLoginContextProvider, useUserLogin }
